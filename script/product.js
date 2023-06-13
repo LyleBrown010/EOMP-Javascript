bf91c02ec65ccfa50bb73d14c275f920f482764e
LyleBrown010 <lylebrown010@gmail.com>
2023-06-13 09:53:06 +0200

    created shell, add meat

---
+++ script/product.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    },
+    {
+        id: 2 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    },
+    {
+        id: 3 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    },
+    {
+        id: 4 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    },
+    {
+        id: 5 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    },
+    {
+        id: 6 , 
+        image: "", 
+        name:"",
+        price:"",
+        category:"", 
+        quantity: ""
+    }
+];
