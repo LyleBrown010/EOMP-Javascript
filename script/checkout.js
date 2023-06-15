5618f9230bca2eb23250490472410cb9a7280b92
LyleBrown010 <lylebrown010@gmail.com>
2023-06-15 02:04:24 +0200

    almost done

--- script/checkout.js
+++ script/checkout.js
@@ -1,1 +1,39 @@
-let cart = JSON.parse(localStorage.getItem(''))
+let cart = JSON.parse(localStorage.getItem('Products')); 
+let checkoutList = document.getElementById('checkout'); 
+
+function cartRemove(index){
+    cart.splice(index, 1)[0];
+    checkoutList();
+}
+
+function checkout(){
+    checkoutList.innerHTML = "";
+
+    cart.forEach((products, index) => {
+        let cartList = document.createElement("div"); 
+        cartList.innerHTML = '' ; 
+        cartList.innerHTML = `
+        <img src ${products.image}></img>
+        <h4>${products.name}</h4>
+        <p>${products.price}</p>
+        `;
+
+        checkoutList.appendChild(cartList);
+    })
+    total(); 
+
+}
+
+checkout(); 
+
+function total(){
+    let price = document.getElementById('total-amount'); 
+    let total = 0; 
+
+    price.forEach((products) => {
+        total += eval(products.price);
+    });
+
+    price.textContent = `Total Price: R${(totalCash)}`
+}
+
